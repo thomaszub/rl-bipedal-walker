4162c5338904c1fb5cf2c31dd271a19056fbaaba
thomaszub <thomas.zub@outlook.de>
2022-05-05 20:54:33 +0200

    Implemented data inserting, replaced loader with dataset

--- rl-bipedal-walker/replay_buffer.py
+++ rl-bipedal-walker/replay_buffer.py
@@ -1,41 +1,46 @@
 from dataclasses import dataclass
+from typing import Tuple
 
 import numpy as np
 import torch
 from omegaconf import DictConfig
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import TensorDataset
 
 
 @dataclass()
 class ReplayBufferConfig:
     size: int
-    batch_size: int
-    shuffle: bool
 
     @staticmethod
     def fromDictConfig(config: DictConfig) -> "ReplayBufferConfig":
-        return ReplayBufferConfig(config.size, config.batch_size, config.shuffle)
+        return ReplayBufferConfig(config.size)
 
 
 class ReplayBuffer:
-    def __init__(self, config: ReplayBufferConfig) -> None:
+    def __init__(
+        self,
+        config: ReplayBufferConfig,
+        input_shape: Tuple[int],
+        target_shape: Tuple[int],
+    ) -> None:
         self.config = config
-        self._replay_buffer_input = []
-        self._replay_buffer_target = []
-
-    def clear(self) -> None:
-        self._replay_buffer_input = []
-        self._replay_buffer_target = []
+        self._replay_buffer_input = np.zeros((self.config.size,) + input_shape)
+        self._replay_buffer_target = np.zeros((self.config.size,) + target_shape)
+        self._next_entry_id = 0
+        self._is_full = False
 
     def add(self, input: np.ndarray, target: np.ndarray) -> None:
-        self._replay_buffer_input.append(input)
-        self._replay_buffer_target.append(target)
-        # TODO Determine insert position
+        self._replay_buffer_input[self._next_entry_id] = input
+        self._replay_buffer_target[self._next_entry_id] = target
+        self._next_entry_id += 1
+        if self._next_entry_id >= self.config.size:
+            self._next_entry_id = 0
+            self._is_full = True
 
-    def loader(self) -> DataLoader:
-        input = torch.tensor(np.array(self._replay_buffer_input)).float()
-        target = torch.tensor(np.array(self._replay_buffer_target)).float()
-        dataset = TensorDataset(input, target)
-        return DataLoader(
-            dataset, batch_size=self.config.batch_size, shuffle=self.config.shuffle
-        )
+    def is_full(self) -> bool:
+        return self._is_full
+
+    def dataset(self) -> TensorDataset:
+        input = torch.tensor(self._replay_buffer_input).float()
+        target = torch.tensor(self._replay_buffer_target).float()
+        return TensorDataset(input, target)
