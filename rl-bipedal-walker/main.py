284e7c3c4b02f9d3f98932c1873be36ff9cde708
thomaszub <thomas.zub@outlook.de>
2022-05-20 20:54:02 +0200

    Refactored training loop

--- rl-bipedal-walker/main.py
+++ rl-bipedal-walker/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import gym
 import hydra
 from omegaconf import DictConfig
@@ -7,20 +9,27 @@ from agent import DDPGAgent, DDPGAgentConfig
 from replay_buffer import ReplayBuffer, ReplayBufferConfig
 
 
-def run(env: gym.Env, agent: DDPGAgent, render: bool) -> float:
-    state = env.reset()
-    done = False
-    sum_reward = 0
-    while not done:
-        if render:
-            env.render()
-        action = agent.action(state)
-        new_state, reward, done, _ = env.step(action)
-        agent.update(state, action, reward, done, new_state)
-        sum_reward += reward
-        state = new_state
+def train(env: gym.Env, agent: DDPGAgent, steps: int) -> List[float]:
+    rewards = []
+    state, done, sum_reward = env.reset(), False, 0
+    curr_episode = 1
+    with trange(0, steps) as tr:
+        tr.set_postfix(curr_episode=curr_episode, last_sum_reward=sum_reward)
+        for _ in tr:
 
-    return sum_reward
+            action = agent.action(state)
+            new_state, reward, done, _ = env.step(action)
+            sum_reward += reward
+            agent.update(state, action, reward, done, new_state)
+            if done:
+                curr_episode += 1
+                rewards.append(sum_reward)
+                tr.set_postfix(curr_episode=curr_episode, last_sum_reward=sum_reward)
+                state, done, sum_reward = env.reset(), False, 0
+            else:
+                state = new_state
+
+    return rewards
 
 
 @hydra.main(config_path="../conf", config_name="config")
@@ -36,12 +45,8 @@ def main(cfg: DictConfig):
     agent = DDPGAgent(agent_config, env.action_space, buffer_state_action)
     agent.train_mode(True)
 
-    rewards = []
-    with trange(0, cfg.training.episodes) as tr:
-        for _ in tr:
-            reward = run(env, agent, False)
-            rewards.append(reward)
-            tr.set_postfix(reward=reward)
+    rewards = train(env, agent, cfg.training.steps)
+
     print("rewards:\n")
     print("\n".join(map(lambda r: str(r), rewards)))
 
