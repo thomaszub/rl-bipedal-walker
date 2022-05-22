0026a15896129d723e2cb6b00e298d94f4a71544
thomaszub <thomas.zub@outlook.de>
2022-05-22 07:50:36 +0200

    Added Layer classes and relu

--- rl-bipedal-walker/core.py
+++ rl-bipedal-walker/core.py
@@ -1,4 +1,12 @@
+from typing import Callable, List, Tuple
+
+import numpy as np
+import numpy.typing as npt
 import torch
+
+Layer = Callable[[npt.NDArray[np.float32]], npt.NDArray[np.float32]]
+
+Activation = Callable[[npt.NDArray[np.float32]], npt.NDArray[np.float32]]
 
 
 def polyak_update(
@@ -6,3 +14,33 @@ def polyak_update(
 ) -> None:
     for p_tgt, p_src in zip(target.parameters(), source.parameters()):
         p_tgt.copy_(polyak * p_tgt + (1.0 - polyak) * p_src)
+
+
+def relu(x: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+    return np.maximum(x, 0)
+
+
+class LinearLayer(Layer):
+    def __init__(self, size: Tuple[int, int], activation: Activation = None) -> None:
+        self.W = np.random.uniform(-1.0, 1.0, size=(size[0], size[1])).astype(
+            np.float32
+        )
+        self.b = np.random.uniform(-1.0, 1.0, size=size[1]).astype(np.float32)
+        self.activation = activation
+        if activation is not None:
+            self._func = lambda x: self.activation(np.add(np.matmul(x, self.W), self.b))
+        else:
+            self._func = lambda x: np.add(np.matmul(x, self.W), self.b)
+
+    def __call__(self, input: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+        return self._func(input)
+
+
+class SequentialLayer(Layer):
+    def __init__(self, layers: List[Layer]) -> None:
+        self._layers = layers
+
+    def __call__(self, input: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+        for layer in self._layers:
+            input = layer(input)
+        return input
