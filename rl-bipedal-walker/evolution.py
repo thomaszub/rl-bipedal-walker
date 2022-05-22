6a074b8d79a1f3e271efda1568d86dfa4cda247d
thomaszub <thomas.zub@outlook.de>
2022-05-22 12:18:56 +0200

    Parallel children generation

--- rl-bipedal-walker/evolution.py
+++ rl-bipedal-walker/evolution.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from copy import deepcopy
 from dataclasses import dataclass
 from typing import List
@@ -44,22 +45,28 @@ class EvolutionalAgent(Agent):
         return self._policy_model(state)
 
     def train(self, env: gym.Env) -> List[float]:
+        executor = ProcessPoolExecutor()
         rewards = []
-        sum_reward = 0
         parent_replaced = 0
         init_parent = deepcopy(self._policy_model)
-        parent = Candidate(init_parent, self._play(env, init_parent))
+        parent = Candidate(init_parent, EvolutionalAgent._play(env, init_parent))
         with trange(0, self.config.generations) as tgen:
             for generation in tgen:
                 tgen.set_postfix(
                     generation=generation,
                     fitness=parent.fitness,
                 )
-                children: List[Candidate] = []
-                for _ in range(self.config.mutations_per_parent):
-                    child = self._mutate(parent.model)
-                    fitness = self._play(env, child)
-                    children.append(Candidate(child, fitness))
+
+                children_f = [
+                    executor.submit(
+                        EvolutionalAgent._create_candidate,
+                        env,
+                        parent.model,
+                        self.config.mutation_strength,
+                    )
+                    for _ in range(self.config.mutations_per_parent)
+                ]
+                children = [child_f.result() for child_f in as_completed(children_f)]
 
                 fitnesses = [child.fitness for child in children]
                 fittest_child = children[np.argmax(fitnesses)]
@@ -67,14 +74,26 @@ class EvolutionalAgent(Agent):
                     parent = fittest_child
                     parent_replaced += 1
 
-                sum_reward = self._play(env, parent.model)
-
-                rewards.append(sum_reward)
+                rewards.append(parent.fitness)
 
         self._policy_model = parent.model
+        executor.shutdown()
         return rewards
 
-    def _play(self, env: gym.Env, policy_model: Layer) -> float:
+    @staticmethod
+    def _create_candidate(
+        env: gym.Env, parent: Model, mutation_strength: float
+    ) -> Candidate:
+        env_c = deepcopy(env)
+        child = deepcopy(parent)
+        for layer in child._layers:
+            layer.W += np.random.normal(scale=mutation_strength, size=layer.W.shape)
+            layer.b += np.random.normal(scale=mutation_strength, size=layer.b.shape)
+        fitness = EvolutionalAgent._play(env_c, child)
+        return Candidate(child, fitness)
+
+    @staticmethod
+    def _play(env: gym.Env, policy_model: Layer) -> float:
         state, done, sum_reward = env.reset(), False, 0
         while not done:
             action = policy_model(state)
@@ -83,14 +102,3 @@ class EvolutionalAgent(Agent):
             state = new_state
 
         return sum_reward
-
-    def _mutate(self, model: Model) -> Model:
-        base = deepcopy(model)
-        for layer in base._layers:
-            layer.W += np.random.normal(
-                scale=self.config.mutation_strength, size=layer.W.shape
-            )
-            layer.b += np.random.normal(
-                scale=self.config.mutation_strength, size=layer.b.shape
-            )
-        return base
