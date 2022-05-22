7bfde014b1f4d738610ab477a68352cabf807834
thomaszub <thomas.zub@outlook.de>
2022-05-22 11:21:38 +0200

    Train by mutating and selection

--- rl-bipedal-walker/evolution.py
+++ rl-bipedal-walker/evolution.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from dataclasses import dataclass
 from typing import List
 
@@ -8,22 +9,32 @@ from omegaconf import DictConfig
 from tqdm import trange
 
 from agent import Agent
-from core import Layer, LinearLayer, SequentialLayer, relu
+from core import Layer, LinearLayer, Model, relu
+
+
+@dataclass(frozen=True)
+class Candidate:
+    model: Layer
+    fitness: float
 
 
 @dataclass()
 class EvolutionalAgentConfig:
     generations: int
+    mutations_per_parent: int
+    mutation_strength: float
 
     @staticmethod
     def fromDictConfig(config: DictConfig) -> "EvolutionalAgentConfig":
-        return EvolutionalAgentConfig(config.generations)
+        return EvolutionalAgentConfig(
+            config.generations, config.mutations_per_parent, config.mutation_strength
+        )
 
 
 class EvolutionalAgent(Agent):
     def __init__(self, config: EvolutionalAgentConfig) -> None:
         self.config = config
-        self._policy_model = SequentialLayer(
+        self._policy_model = Model(
             LinearLayer(24, 64, activation=relu),
             LinearLayer(64, 32, activation=relu),
             LinearLayer(32, 4, activation=np.tanh),
@@ -35,14 +46,32 @@ class EvolutionalAgent(Agent):
     def train(self, env: gym.Env) -> List[float]:
         rewards = []
         sum_reward = 0
+        parent_replaced = 0
+        init_parent = deepcopy(self._policy_model)
+        parent = Candidate(init_parent, self._play(env, init_parent))
         with trange(0, self.config.generations) as tgen:
             for generation in tgen:
-                tgen.set_postfix(generation=generation, last_sum_reward=sum_reward)
+                tgen.set_postfix(
+                    generation=generation,
+                    fitness=parent.fitness,
+                )
+                children: List[Candidate] = []
+                for _ in range(self.config.mutations_per_parent):
+                    child = self._mutate(parent.model)
+                    fitness = self._play(env, child)
+                    children.append(Candidate(child, fitness))
 
-                sum_reward = self._play(env, self._policy_model)
+                fitnesses = [child.fitness for child in children]
+                fittest_child = children[np.argmax(fitnesses)]
+                if fittest_child.fitness > parent.fitness:
+                    parent = fittest_child
+                    parent_replaced += 1
+
+                sum_reward = self._play(env, parent.model)
 
                 rewards.append(sum_reward)
 
+        self._policy_model = parent.model
         return rewards
 
     def _play(self, env: gym.Env, policy_model: Layer) -> float:
@@ -54,3 +83,14 @@ class EvolutionalAgent(Agent):
             state = new_state
 
         return sum_reward
+
+    def _mutate(self, model: Model) -> Model:
+        base = deepcopy(model)
+        for layer in base._layers:
+            layer.W += np.random.normal(
+                scale=self.config.mutation_strength, size=layer.W.shape
+            )
+            layer.b += np.random.normal(
+                scale=self.config.mutation_strength, size=layer.b.shape
+            )
+        return base
